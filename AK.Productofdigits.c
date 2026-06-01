68a41b72cf96099fc8fb458f510b34c35ee214a5
AaryanshKumar <25116cn112@glbitm.ac.in>
2026-06-01 16:20:11 +0530

    Add files via upload

---
+++ AK.Productofdigits.c
@@ -0,0 +1,21 @@
+
+#include <stdio.h>
+
+int main()
+{
+    int num, product = 1, digit;
+
+    printf("Enter any number : ");
+    scanf("%d", &num);
+
+    while(num != 0)
+    {
+        digit = num % 10;
+        product = product * digit;
+        num = num / 10;
+    }
+
+    printf("Product of given number is = %d", product);
+    return 0;
+}
+
